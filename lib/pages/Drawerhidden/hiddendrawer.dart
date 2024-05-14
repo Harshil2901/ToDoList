5037d5fcee337f9e20c811abcd1446f2153506db
Harshil2901 <sutharharshil01@gmail.com>
2024-05-15 01:53:40 +0530

    hiddendrawer page

---
+++ lib/pages/Drawerhidden/hiddendrawer.dart
@@ -0,0 +1,33 @@
+import "package:flutter/material.dart";
+import 'package:todolist/pages/Drawerhidden/hiddendrawer.dart';
+import 'package:provider/provider.dart';
+import 'package:todolist/data/shared/Task_saved.dart';
+import 'package:todolist/data/themes.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await TaskerPreference.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+
+          return MaterialApp(
+            title: "Fluttter Demo",
+            debugShowCheckedModeBanner: false,
+            themeMode: themeProvider.themeMode,
+            darkTheme: Mytheme.darkthem,
+            theme: Mytheme.lighttheme,
+            home: HiddenDrawer(),
+          );
+        },
+      );
+}

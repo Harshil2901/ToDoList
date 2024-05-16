4100507a50c4533c1bac6c304bbae4618d93456e
Harshil2901 <sutharharshil01@gmail.com>
2024-05-17 04:56:09 +0530

    drawer item

--- lib/pages/Drawerhidden/hiddendrawer.dart
+++ lib/pages/Drawerhidden/hiddendrawer.dart
@@ -1,33 +1,33 @@
-import "package:flutter/material.dart";
-import 'package:todolist/pages/Drawerhidden/hiddendrawer.dart';
-import 'package:provider/provider.dart';
-import 'package:todolist/data/shared/Task_saved.dart';
-import 'package:todolist/data/themes.dart';
+// import "package:flutter/material.dart";
+// import 'package:todolist/pages/Drawerhidden/hiddendrawer.dart';
+// import 'package:provider/provider.dart';
+// import 'package:todolist/data/shared/Task_saved.dart';
+// import 'package:todolist/data/themes.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
 
-  await TaskerPreference.init();
-  runApp(const MyApp());
-}
+//   await TaskerPreference.init();
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) => ChangeNotifierProvider(
-        create: (context) => ThemeProvider(),
-        builder: (context, _) {
-          final themeProvider = Provider.of<ThemeProvider>(context);
+//   @override
+//   Widget build(BuildContext context) => ChangeNotifierProvider(
+//         create: (context) => ThemeProvider(),
+//         builder: (context, _) {
+//           final themeProvider = Provider.of<ThemeProvider>(context);
 
-          return MaterialApp(
-            title: "Fluttter Demo",
-            debugShowCheckedModeBanner: false,
-            themeMode: themeProvider.themeMode,
-            darkTheme: Mytheme.darktheme,
-            theme: Mytheme.lighttheme,
-            home: HiddenDrawer(),
-          );
-        },
-      );
-}
+//           return MaterialApp(
+//             title: "Fluttter Demo",
+//             debugShowCheckedModeBanner: false,
+//             themeMode: themeProvider.themeMode,
+//             darkTheme: Mytheme.darktheme,
+//             theme: Mytheme.lighttheme,
+//             home: HiddenDrawer(),
+//           );
+//         },
+//       );
+// }

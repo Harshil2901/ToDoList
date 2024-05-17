ab4bdb1d2fd8510620281abfdeec463f667f4a4f
Harshil2901 <sutharharshil01@gmail.com>
2024-05-18 02:00:55 +0530

    hiddendrawer page

--- lib/pages/Drawerhidden/hiddendrawer.dart
+++ lib/pages/Drawerhidden/hiddendrawer.dart
@@ -1,33 +1,78 @@
-// import "package:flutter/material.dart";
-// import 'package:todolist/pages/Drawerhidden/hiddendrawer.dart';
-// import 'package:provider/provider.dart';
-// import 'package:todolist/data/shared/Task_saved.dart';
-// import 'package:todolist/data/themes.dart';
+import 'package:flutter/material.dart';
+import 'package:todolist/pages/homepage.dart';
+import 'package:todolist/pages/Drawerhidden/Drawer_widget.dart';
+import 'package:flutter/services.dart';
 
-// Future main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
+class HiddenDrawer extends StatefulWidget {
+  const HiddenDrawer({super.key});
 
-//   await TaskerPreference.init();
-//   runApp(const MyApp());
-// }
+  @override
+  State<HiddenDrawer> createState() => _HiddenDrawerState();
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
+class _HiddenDrawerState extends State<HiddenDrawer> {
+  late double xOffset;
+  late double yOffset;
+  late double scaleFactor;
+  late bool isDrawingOpen;
+  bool isDragging = false;
 
-//   @override
-//   Widget build(BuildContext context) => ChangeNotifierProvider(
-//         create: (context) => ThemeProvider(),
-//         builder: (context, _) {
-//           final themeProvider = Provider.of<ThemeProvider>(context);
+  void openDrawer() {
+    setState(() {
+      xOffset = 300;
+      yOffset = 70;
+      scaleFactor = 0.85;
+      isDrawingOpen = true;
+    });
+  }
 
-//           return MaterialApp(
-//             title: "Fluttter Demo",
-//             debugShowCheckedModeBanner: false,
-//             themeMode: themeProvider.themeMode,
-//             darkTheme: Mytheme.darktheme,
-//             theme: Mytheme.lighttheme,
-//             home: HiddenDrawer(),
-//           );
-//         },
-//       );
-// }
+  void closeDrawer() {
+    setState(() {
+      xOffset = 0;
+      yOffset = 0;
+      scaleFactor = 1;
+      isDrawingOpen = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    closeDrawer();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: const Color(0xFF04123F),
+        body: Stack(
+          children: [DrawerWidget(closedDrawer: closeDrawer), buildpage()],
+        ),
+      );
+
+  Widget buildpage() {
+    return GestureDetector(
+      onHorizontalDragStart: (details) => isDragging = true,
+      onHorizontalDragUpdate: (details) {
+        if (isDragging) return;
+        const delta = 1;
+        if (details.delta.dx > delta) {
+          openDrawer();
+        } else if (details.delta.dx < delta) {
+          closeDrawer();
+        }
+      },
+      onTap: closeDrawer,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        transform: Matrix4.translationValues(xOffset, yOffset, 0)
+          ..scale(scaleFactor),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(isDrawingOpen ? 30 : 0),
+          child: MyHomePage(opendrawer: openDrawer),
+        ),
+      ),
+    );
+  }
+}
